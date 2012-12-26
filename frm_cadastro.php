d262b3af0a8f91e3b2f72741c85889b861b26626
Alexandre Sampaio <allexonrails@gmail.com>
2012-12-25 23:12:31 -0200

    Primeiro Impacto

---
+++ frm_cadastro.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<HTML LANG="pt-br">
+<HEAD>
+ <TITLE>Social-Network - Cadastro</TITLE>
+ <LINK REL="stylesheet" HREF="css/cadastro.css" TYPE="text/css" />
+</HEAD>
+<BODY>
+      <DIV ID="topo">
+           <DIV ID="cAlign">
+                <A HREF="#">
+                   <IMG SRC="images/logo.png" ALT="social-network.com" />
+                   <SPAN>
+                         <A HREF="#">Portal</A> | <A HREF="#">Forum</A>
+                   </SPAN>
+                </A>
+           </DIV><!-- cAlign -->
+      </DIV><!-- topo -->
+      <DIV ID="cAlign">
+           <DIV ID="content">
+                <DIV ID="left">
+                </DIV>
+           
+                <DIV ID="formulario">
+                </DIV><!-- formulario -->
+           </DIV><!-- content -->
+       </DIV><!-- cAlign -->
+
+</BODY>
+</HTML>
