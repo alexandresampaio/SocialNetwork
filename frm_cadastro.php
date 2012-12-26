95cb36077c8da7c60f3d41172dc9695d3ba77527
Alexandre Sampaio <allexonrails@gmail.com>
2012-12-25 23:33:10 -0200

    Ajuste do frm_cadastro

--- frm_cadastro.php
+++ frm_cadastro.php
@@ -18,9 +18,65 @@
       <DIV ID="cAlign">
            <DIV ID="content">
                 <DIV ID="left">
-                </DIV>
+                     <UL>
+                         <li>eu sou</li>
+                         <li>data de nascimento</li>
+                         <li>meu e-mail</li>
+                         <li>nova senha</li>
+                         <li>verificação contra fraudes</li>
+                     </UL>
+                </DIV><!-- left -->
            
                 <DIV ID="formulario">
+                     <FORM NAME="cadastro" METHOD="post" ACTION="" >
+                           <div>
+                                <div class="inputFloat">
+                                     <span>nome</span>
+                                     <input type="text" name="nome" class="inputText" />
+                                </div>
+                                
+                                <div class="inputFloat">
+                                     <span>sobrenome</span>
+                                     <input type="text" name="sobrenome" class="inputText" />
+                                </div>
+                           </div>
+                           
+                           <SPAN class="spanHide">eu sou</SPAN>
+                           <select name="sexo">
+                                   <option value="">Selecione seu Genero</option>
+                           </select>
+                           
+                           <SPAN class="spanHide">Data de nascimento</SPAN>
+                           <select name="dia">
+                                   <option value="">Dia</option>
+                           </select>
+                           <select name="mes">
+                                   <option value="">Mes</option>
+                           </select>
+                           <select name="ano">
+                                   <option value="">ano</option>
+                           </select>
+                           
+                           <span>seu e-mail</span>
+                           <input type="text" name="email" class="inputText" />
+                           
+                           <span>nova senha</span>
+                           <input type="text" name="senha" class="inputText" />
+                           
+                           <div>
+                                <div class="captchaFloat">
+                                     <IMG SRC="#" width="200" height="60" alt="captcha" />
+                                </div>
+                                
+                                <div class="captchaFloat">
+                                     <SPAN>digite os caracteres</SPAN>
+                                     <input type="text" name="palavra" class="inputText" />
+                                </div>
+                           </div>
+                           <SPAN>&nbsp</SPAN>
+                           <input type="submit" name="cadastrar" />
+
+                     </FORM>
                 </DIV><!-- formulario -->
            </DIV><!-- content -->
        </DIV><!-- cAlign -->
