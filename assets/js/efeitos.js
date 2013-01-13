6adcb6b585149f85376a3effd7fe74194166a412
Alexandre Sampaio <assassin.alex.creed.proj@gmailcom>
2013-01-13 15:07:24 -0200

    Alterações

---
+++ assets/js/efeitos.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function($){
+// Efeitos fade jquery
+
+$("#Erro").fadeIn(3000);
+$("#Erro").fadeOut(5000);
+
+// Efeitos fade jquery
+
+$("#aviso").fadeIn(3000);
+$("#aviso").fadeOut(5000);
+
+
+
+});
