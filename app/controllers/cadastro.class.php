458f60786fb0262dd202f7ce8ca8fce58b86b011
Alexandre Sampaio <assassin.alex.creed.proj@gmailcom>
2013-01-15 17:57:21 -0200

    Ajuste do layout

---
+++ app/controllers/cadastro.class.php
@@ -0,0 +1,106 @@
+<?php
+    class Cadastro extends DB
+    {
+        public $erro;
+        private $dados;
+        private $_tabela;
+        
+        function __construct(array $dados, $table="usuarios")
+        {
+            $this->_tabela=$table;
+            $this->dados=$dados;
+            
+            $this->register();
+        }
+        
+        private function encrypt($senha)
+        {
+            return sha1($senha);
+        }
+        
+        private function check ($email)
+        {
+            $check = self::getConn()->prepare('SELECT `id` FROM `'.$this->_tabela.'` WHERE `email`=? LIMIT 1');
+            /*
+            Dessa forma ou da forma abaixo
+            
+            $check->execute(array($email));
+            return $check->rowCount() ==0 ? true : false;
+
+            -> Seguindo essa forma retirar o !$this da
+            condição do register.
+
+            */
+            $check->execute(array($email));
+			//pode ser array($email) ou 
+			//$check->execute(array($this->dados['email']));
+            return $check->rowCount();
+        }
+        
+        private function validar()
+        {
+            if(strtolower($this->dados['captcha']) <> strtolower($_SESSION['captchaCadastro']))
+            {
+                $this->erro[] = 'O c&oacute;digo informado n&atilde;o &eacute; v&aacute;lido!';
+            }
+            
+            foreach($this->dados as $column=>$value)
+            {
+                if($column=='senha')
+                {
+                    $this->dados[$column] = $this->encrypt($value);
+                }
+
+                if($value == '')
+                {
+                    $this->erro[] = 'O Campo <STRONG>'.$column.'</STRONG> é obrigat&oacute;rio!';
+                }
+
+            }
+
+            if($column=='email' AND !preg_match("/^[a-z0-9_\.\-]+@[a-z0-9_\.\-]+\.[a-z]{2,4}$/i", $value ))
+            {
+               $this->erro[] = 'O email informado n&atilde;o &eacute; v&aacute;lido!';
+            }
+		}
+        
+        private function setCampos()
+        {
+            return '`'.implode('`=?, `',array_keys($this->dados)).'`=?';
+        }
+        
+        
+        function register()
+        {
+            $this->validar();
+            if(!$this->check($this->dados['email']))
+            {
+                if(empty($this->erro))
+                {
+                   unset($this->dados['captcha']);
+
+                   $INSERIR=self::getConn()->prepare('INSERT INTO '.$this->_tabela.' SET '.$this->setCampos().', `cadastro`=NOW()');
+                   if($INSERIR->execute(array_values($this->dados)))
+                   {
+                        header('Location: ./');
+                        exit();
+                   }
+                   else
+                   {
+                        $this->erro[] = 'N&atilde;o foi posssivel realizar seu cadastro, tente mais tarde!';
+                   }
+                }
+            }
+            else
+            {
+               $this->erro[] = 'O email informado j&aacute est&aacute cadastrado!';
+            }
+        }
+        
+        function getErros()
+        {
+            return implode('<br />',$this->erro);
+        }
+
+    }
+?>
--- index.php
+++ index.php
@@ -1,30 +1,55 @@
-<?php
-		include('controllers/db/db.class.php');
-		include('controllers/login.class.php');
-	
-		$objLogin = new Login;
-		
-		if(!$objLogin->logado())
-		{
-			include('login.php');
-			exit();	
-		}
-		
-		if(isset($_GET['sair']) AND $_GET['sair']==true)
-		{
-			$objLogin->sair();
-			header('Location: ./');
-		}
-?>
+<?php include('shared/header.php'); ?>        
+        <div id="amarra-center-left">
+        
+            <div class="center">
+               
+                <div class="blocos" id="dexar-recados">
+                    <h1><?php echo $user_nome.' '.$user_sobrenome ?></h1>
+                    
+                    <form name="dexar-recado" action="" method="post" enctype="multipart/form-data">
+                        <input type="text" class="inputTxt" name="recado" value="Deixe um recado para seus amigos"  onfocus="if(this.value=='Deixe um recado para seus amigos')this.value='';" onblur="if(this.value=='')this.value='Deixe um recado para seus amigos';" /><input class="inputSub" type="submit" value="postar" />
+                    </form>
+                </div><!--blocos-->
 
+                <div class="blocos" id="pagina">
+                	<h2>atualizações</h2>
+                </div><!--blocos-->
+                
+            </div><!--center-->
+            
+            <div class="right">
+            
+                <div class="blocos" id="publicidade">
+                    <img src="midias/banner.gif" />
+                </div><!--blocos-->
+                
+                <div class="blocos" id="meus-amigos">
+                    <span>meus amigos(18) <a href="#">todos</a></span>
+                    <ul>
+                        <li><a href="#"><img src="midias/user.jpg" alt="" title="usuario" /></a></li>
+                        <li><a href="#"><img src="midias/user.jpg" alt="" title="usuario" /></a></li>
+                        <li><a href="#"><img src="midias/user.jpg" alt="" title="usuario" /></a></li>
+                        <li><a href="#"><img src="midias/user.jpg" alt="" title="usuario" /></a></li>
+                        <li><a href="#"><img src="midias/user.jpg" alt="" title="usuario" /></a></li>
+                        <li><a href="#"><img src="midias/user.jpg" alt="" title="usuario" /></a></li>
+                        <li><a href="#"><img src="midias/user.jpg" alt="" title="usuario" /></a></li>
+                        <li><a href="#"><img src="midias/user.jpg" alt="" title="usuario" /></a></li>
+                        <li><a href="#"><img src="midias/user.jpg" alt="" title="usuario" /></a></li>
+                        <li><a href="#"><img src="midias/user.jpg" alt="" title="usuario" /></a></li>
+                        <li><a href="#"><img src="midias/user.jpg" alt="" title="usuario" /></a></li>
+                        <li><a href="#"><img src="midias/user.jpg" alt="" title="usuario" /></a></li>
+                        <li><a href="#"><img src="midias/user.jpg" alt="" title="usuario" /></a></li>
+                        <li><a href="#"><img src="midias/user.jpg" alt="" title="usuario" /></a></li>
+                        <li><a href="#"><img src="midias/user.jpg" alt="" title="usuario" /></a></li>
+                        <li><a href="#"><img src="midias/user.jpg" alt="" title="usuario" /></a></li>
+                        <li><a href="#"><img src="midias/user.jpg" alt="" title="usuario" /></a></li>
+                        <li><a href="#"><img src="midias/user.jpg" alt="" title="usuario" /></a></li>
+                    </ul>
+                </div><!--blocos-->
+                                
+            </div><!--right-->
 
-<html LANG="pt-br">
-<head>
-<meta charset="utf-8" />
-<title>SOCIAL-NETWORK - HOME</title>
-</head>
-
-<body>
-	Logado:<a href="?sair=true">Sair</a>
-</body>
-</html>
+                    
+        </div><!--amarra-center-left-->
+        
+<?php include('shared/footer.php'); ?>
